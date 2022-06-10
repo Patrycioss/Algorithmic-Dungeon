98246dc5beb1a3670646ec4a97efc85e17d39398
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-06-10 08:44:26 +0200

    Excellentdungeon v1

--- sources/Solution/NodeGraphGenerators/ExcellentDungeonNodeGraphV1.cs
+++ sources/Solution/NodeGraphGenerators/ExcellentDungeonNodeGraphV1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using Saxion.CMGT.Algorithms.sources.Assignment.Dungeon;
 using Saxion.CMGT.Algorithms.sources.Assignment.NodeGraph;
@@ -9,43 +8,14 @@ namespace Saxion.CMGT.Algorithms.sources.Solution.NodeGraphGenerators;
 internal class ExcellentDungeonNodeGraphV1 : NodeGraph
 {
 	private readonly Dungeon dungeon;
-	private readonly List<Room> rooms;
-	private readonly List<Door> doors;
-
-	private Dictionary<Door, Node> doorNodes;
 
 	public ExcellentDungeonNodeGraphV1(Dungeon pDungeon) : base((int)(pDungeon.size.Width * pDungeon.scale), (int)(pDungeon.size.Height * pDungeon.scale), (int)pDungeon.scale/3)
 	{
 		dungeon = pDungeon;
-		rooms = dungeon.rooms;
-		doors = dungeon.doors;
-		doorNodes = new Dictionary<Door, Node>();
 	}
 
 	protected override void ActualGenerate()
 	{
-		// foreach (Door door in doors)
-		// {
-		// 	Node node = new(GetDoorCenter(door));
-		// 	nodes.Add(node);
-		// 	
-		// 	doorNodes.Add(door,node);
-		// }
-		//
-		// foreach (Room room in rooms)
-		// {
-		// 	Node roomNode = new(GetRoomCenter(room));
-		// 	nodes.Add(roomNode);
-		// 	
-		// 	foreach (Door door in room.doors)
-		// 	{
-		// 		AddConnection(roomNode,doorNodes[door]);
-		// 	}
-		// }
-
-		
-		
-		
 		foreach (Room room in dungeon.rooms)
 		{
 			Console.WriteLine($"TL: {room.topLeft},TR: {room.topRight}, BL: {room.bottomLeft}, BR: {room.bottomRight}");
@@ -54,21 +24,55 @@ internal class ExcellentDungeonNodeGraphV1 : NodeGraph
 			{
 				for (int j = room.topRight.Y + 1; j < room.bottomLeft.Y; j++)
 				{
-					Node node = new Node(GetPointCenter(new Point(i,j)));
+					Node node = new(GetPointCenter(new Point(i,j)));
 					nodes.Add(node);
 				}
 			}
 		}
-		
+
+		foreach (Door door in dungeon.doors)
+		{
+			Node node = new(GetDoorCenter(door));
+			nodes.Add(node);
+		}
+
+		int o = (int)dungeon.scale;
+
+		for (int i = nodes.Count-1; i >= 0; i--)
+		{
+			Node nodeA = nodes[i];
+			
+			for (int j = i - 1; j >= 0; j--)
+			{
+				Node nodeB = nodes[j];
+				
+				//Left
+				if (nodeA.location.X - o == nodeB.location.X && nodeA.location.Y == nodeB.location.Y)
+				{
+					AddConnection(nodeA,nodeB);
+					continue;
+				}
+				//Right
+				if (nodeA.location.X + o == nodeB.location.X && nodeA.location.Y == nodeB.location.Y)
+				{
+					AddConnection(nodeA,nodeB);
+					continue;
+				}
+				//Top
+				if (nodeA.location.X == nodeB.location.X && nodeA.location.Y - o == nodeB.location.Y)
+				{
+					AddConnection(nodeA,nodeB);
+					continue;
+				}
+				//Bottom
+				if (nodeA.location.X == nodeB.location.X && nodeA.location.Y + o == nodeB.location.Y)
+				{
+					AddConnection(nodeA,nodeB);
+				}
+			}
+		}
 	}
 
-	private Point GetRoomCenter(Room pRoom)
-	{
-		float centerX = (pRoom.area.Left + pRoom.area.Right) / 2.0f * dungeon.scale;
-		float centerY = (pRoom.area.Top + pRoom.area.Bottom) / 2.0f * dungeon.scale;
-		return new Point((int)centerX, (int)centerY);
-	}
-	
 	private Point GetDoorCenter(Door pDoor)
 	{
 		return GetPointCenter(pDoor.location);
